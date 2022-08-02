1780f57da472366fb512fee8f4224e3b2387ce8b
C0deGeek007 <sk773870@gmail.com>
2022-08-02 21:07:43 +0530

    dynamic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,FormBuilder, Form, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+  ngOnInit(): void {
+  }
+
+  constructor(private fb:FormBuilder){}
+
   title = 'dynamicForm';
+
+
+
+  mcqsForm=this.fb.group({
+    mcqs:this.fb.array([])
+  })
+
+  addFormGroup() {
+    const profileForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+    });
+  }
+
+  get mcqs() {
+    return this.mcqsForm.get('mcqs') as FormArray;
+  }
+
+
+  addMcq() {
+    const profileForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+    });
+    this.mcqs.push(profileForm);
+  }
+
+  onSubmit() {
+    console.log(this.mcqsForm);
+    console.log(this.mcqsForm.controls['mcqs'].value);
+  }
+
+  mcqFn(mcq:any) {
+    console.log(mcq);
+  }
+
+  mcqsFn(mcqs:any) {
+    console.log("Array of queations i.e mcqs");
+    console.log(mcqs);
+  }
+
 }
